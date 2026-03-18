6eac72c8b02b5cadb8858cdc23a389e55f3ce281
comethrusws <basabjha.dev@gmail.com>
2026-03-18 08:51:49 +0545

    hahahaha

--- src/main/java/DatabaseConfig.java
+++ src/main/java/DatabaseConfig.java
@@ -3,19 +3,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    // Database credentials
+    // we use env ik. but welp. dis java. and i dont like it, and it's a learning project.
+    // remember kids, always use env vards for creds. im just being lazy rn because this project doesnt matter much. 
+    // the best practice is to not let ppl access ur secret sauce creds. ALWAYS. ciao!
     private static final String URL = "jdbc:postgresql://ep-dawn-resonance-a1m7xvah-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require";
     private static final String USER = "neondb_owner";
     private static final String PASSWORD = "npg_rhYLjKv6SZ1y";
 
     /**
-     * Establishes a connection to the Neon PostgreSQL database.
      * @return Connection object
      * @throws SQLException if a database access error occurs
      */
     public static Connection getConnection() throws SQLException {
         try {
-            // Load the PostgreSQL JDBC driver
             Class.forName("org.postgresql.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
